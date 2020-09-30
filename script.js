8cc4dffbabc059f7cbf45eab80c435ed319a097c
Harshanan Saddanathan <hsaddanathan21@gmail.com>
2020-09-30 19:25:25 -0400

    Added basic functionality of the questions and answers cycling through. Timer works how it should

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 console.log("Hello World!");
 
 var timerScore = document.getElementById("timer");
-
 var mainScreen = document.getElementById("main-screen");
 var startButton = document.getElementById("start-quiz");
+var questionSection = document.getElementById("questions-section");
+var answerChoices = document.getElementById("answers-section");
 var completionPage = document.getElementById("completion-page");
 
 var timeRemaining = 75;
 var interval;
+var questionIndex = 0;
 
-var questionBank = [
+var questionArray = [
   {
     question: "Commonly used data types DO NOT include:",
     choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
@@ -53,13 +55,125 @@ var questionBank = [
     answer: "4. console.log",
   },
 ];
+var answerValidation;
+var finalScore;
 
-completionPage.style.display="none"; 
+completionPage.style.display = "none";
 
+function startTimer() {
+  timerScore.text = "Time: " + timeRemaining;
+  interval = setInterval(function () {
+    timeRemaining--;
+    timerScore.textContent = "Time: " + timeRemaining;
 
-function startQuiz(){
-    // Hides landing page text and button
-    mainScreen.style.display="none";
+    finalScore = timeRemaining;
+    if (timeRemaining === 0) {
+      clearInterval(interval);
+    }
+  }, 1000);
+}
+
+function startQuiz() {
+  // Hides landing page text and button
+  mainScreen.style.display = "none";
+  startTimer();
+  presentQuestions(questionIndex);
+}
+function presentQuestions(index) {
+  if (index === questionArray.length) {
+    //    Ends Game and Presents Score Screen
+    clearInterval(interval);
+    presentScoreScreen();
+  } else {
+    // Clears Answer section before each question
+    answerChoices.innerHTML = "";
+    questionSection.textContent = questionArray[index].question;
+
+    console.log(questionSection.textContent);
+
+    for (var i = 0; i < questionArray[index].choices.length; i++) {
+      var answerBttns = document.createElement("button");
+
+      answerBttns.textContent = questionArray[index].choices[i];
+
+      answerBttns.setAttribute("class", "btn btn-primary rounded");
+      answerBttns.setAttribute("data-index", i);
+
+      answerChoices.appendChild(answerBttns);
+    }function presentQuestions(index) {
+        if (index === questionArray.length) {
+          //    Ends Game and Presents Score Screen
+          clearInterval(interval);
+          presentScoreScreen();
+        } else {
+          // Clears Answer section before each question
+          answerChoices.innerHTML = "";
+          questionSection.textContent = questionArray[index].question;
+      
+          console.log(questionSection.textContent);
+      
+          for (var i = 0; i < questionArray[index].choices.length; i++) {
+            var answerBttns = document.createElement("button");
+      
+            answerBttns.textContent = questionArray[index].choices[i];
+      
+            answerBttns.setAttribute("class", "btn btn-primary rounded");
+            answerBttns.setAttribute("data-index", i);
+      
+            answerChoices.appendChild(answerBttns);
+          }
+        }
+      }
+      function checkAnswer(event){
+          var answerIndex = event.target.getAttribute("data-index");
+          event.preventDefault();
+          
+          if(event.target.matches("button") && 
+          questionArray[questionIndex].choices[answerIndex] === questionArray[questionIndex].answer){
+              answerValidation = "Correct!";
+              console.log(answerValidation);
+              questionIndex++;
+              presentQuestions(questionIndex);
+          }
+          else{
+              answerValidation = "Wrong";
+              console.log(answerValidation);
+              timeRemaining -=15;
+              questionIndex++;
+              presentQuestions(questionIndex);
+          }
+      }
+  }
+}
+function checkAnswer(event) {
+  var answerIndex = event.target.getAttribute("data-index");
+  event.preventDefault();
+
+  if (
+    event.target.matches("button") &&
+    questionArray[questionIndex].choices[answerIndex] ===
+      questionArray[questionIndex].answer
+  ) {
+    answerValidation = "Correct!";
+    console.log(answerValidation);
+    questionIndex++;
+    presentQuestions(questionIndex);
+  } else {
+    answerValidation = "Wrong";
+    console.log(answerValidation);
+    timeRemaining -= 10;
+    questionIndex++;
+    presentQuestions(questionIndex);
+  }
+}
+function presentScoreScreen(){
+    var qaContainer = document.getElementById("qa-container");
+    qaContainer.innerHTML = "";
+    completionPage.style.display = "block";
+
+    var score = document.getElementById("final-score");
+    score.textContent = "Your final score is " + finalScore
 }
 // Event Listeners
-startButton.addEventListener("click",startQuiz);
+startButton.addEventListener("click", startQuiz);
+answerChoices.addEventListener("click", checkAnswer);
